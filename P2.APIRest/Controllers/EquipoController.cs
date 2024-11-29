78031f5ef2c27aafc14f7b8b5f2153290c216dc7
Angel <angeldavidstudios@outlook.com>
2024-11-28 20:57:21 -0500

    Creacion del controlador e inyeccion de dependencias

---
+++ P2.APIRest/Controllers/EquipoController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using P2.APIRest.Repository.Interfaces;
+using P2.Models.Models;
+
+namespace P2.APIRest.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class EquipoController: ControllerBase
+{
+    private readonly IEquipoRepository _equipoRepository;
+    
+    public EquipoController(IEquipoRepository equipoRepository)
+    {
+        _equipoRepository = equipoRepository;
+    }
+    
+    [HttpGet]
+    public IActionResult GetAllEquipos()
+    {
+        return Ok(_equipoRepository.GetAllEquipos());
+    }
+    
+    [HttpGet("{id}")]
+    public IActionResult GetEquipoById(int id)
+    {
+        return Ok(_equipoRepository.GetEquipoById(id));
+    }
+    
+    [HttpPost]
+    public IActionResult AddEquipo([FromBody] Equipos equipo)
+    {
+        _equipoRepository.AddEquipo(equipo);
+        return Ok("Equipo agregado con exito");
+    }
+    
+    [HttpPut]
+    public IActionResult UpdateEquipo([FromBody] Equipos equipo)
+    {
+        _equipoRepository.UpdateEquipo(equipo);
+        return Ok("Equipo actualizado con exito");
+    }
+    
+    [HttpDelete("{id}")]
+    public IActionResult DeleteEquipo(int id)
+    {
+        _equipoRepository.DeleteEquipo(id);
+        return Ok("Equipo eliminado con exito");
+    }
+}
--- P2.APIRest/Program.cs
+++ P2.APIRest/Program.cs
@@ -1,3 +1,6 @@
+using P2.APIRest.Repository;
+using P2.APIRest.Repository.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,7 +10,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 
 // Repositories
-// builder.Services.AddScoped<IEquipoRepository, EquipoRepository>();
+builder.Services.AddScoped<IEquipoRepository, EquipoRepository>();
 
 var app = builder.Build();
 
