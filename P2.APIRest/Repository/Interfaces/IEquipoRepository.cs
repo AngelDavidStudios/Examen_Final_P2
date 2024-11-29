d9c5a669fda3cbe7c6d6165208fda71181e7a7f5
Angel <angeldavidstudios@outlook.com>
2024-11-28 20:46:00 -0500

    Configuracion del program APIs y creacion de la interfaz IEquipoRepository.cs

---
+++ P2.APIRest/Repository/Interfaces/IEquipoRepository.cs
@@ -0,0 +1,14 @@
+﻿using P2.Models.Models;
+
+namespace P2.APIRest.Repository.Interfaces;
+
+public interface IEquipoRepository
+{
+    List<Equipos> GetAllEquipos();
+    Equipos GetEquipoById(int id);
+    
+    // CRUD
+    void AddEquipo(Equipos equipo);
+    void UpdateEquipo(Equipos equipo);
+    void DeleteEquipo(int id);
+}
