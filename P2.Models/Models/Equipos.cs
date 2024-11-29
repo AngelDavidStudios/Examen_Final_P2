0e4d92a95e674e92e9b17a7fe57c1f87b18ae496
Angel <angeldavidstudios@outlook.com>
2024-11-28 20:42:45 -0500

    Modelo de GENERAL.equipos agregado

---
+++ P2.Models/Models/Equipos.cs
@@ -0,0 +1,10 @@
+﻿namespace P2.Models.Models;
+
+public class Equipos
+{
+    public int IdEquipo { get; set; }
+    public string Nombre { get; set; }
+    public string Tipo { get; set; }
+    public double PrecioXDia { get; set; }
+    public string Descripcion { get; set; }
+}
